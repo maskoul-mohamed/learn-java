b8c88ae87fac4493c7ab5982d05529026f32adb2
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-03-22 16:18:53 +0000

    Add variables examples

--- variables/example1.java
+++ variables/example1.java
@@ -4,5 +4,9 @@ public class example1 {
     public static void main(String[] args){
         String name = "Mohamed";
         System.out.println(name);
+
+        int age;
+        age = 25;
+        System.out.println(age);
     }
 }
---
+++ variables/example2.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class example2 {
+    public static void main(String[] args){
+        String firstName = "Mohammed ";
+        String lastName = "Maskoul";
+        String fullName  = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
