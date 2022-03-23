9283681f5c9cf66e22778eeaea496b858791ce54
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-03-23 10:55:25 +0000

    Add characters examples

---
+++ dataTypes/characters/Example1.java
@@ -0,0 +1,13 @@
+package dataTypes.characters;
+
+public class Example1 {
+    public static void main(String[] args) {
+        char myGrade = 'B';
+        char myGrade2 = 'A';
+        char myGrade3 = 'F';
+        System.out.println(myGrade);
+        System.out.println(myGrade2);
+        System.out.println(myGrade3);
+
+    }
+}
