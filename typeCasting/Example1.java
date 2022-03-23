2362c9f75fe557e92be3668e153c3beff5a2b039
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-03-23 12:08:00 +0000

    Add type casting examples

---
+++ typeCasting/Example1.java
@@ -0,0 +1,13 @@
+package typeCasting;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);      // Outputs 9
+        System.out.println(myDouble);   // Outputs 9.0
+
+
+    }
+}
---
+++ typeCasting/Example2.java
@@ -0,0 +1,13 @@
+package typeCasting;
+
+public class Example2 {
+    public static void main(String[] args) {
+        double myDouble = 34.12d;
+        int myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
+
+
+    }
+}
