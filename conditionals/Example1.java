7bb3d1e502a7cd695cd8a6c12185e58f88bcb380
Mohamed Maskoul <maskoul.mohammed@gmail.com>
2022-09-07 19:19:31 +0100

    Add conditionals example 1

---
+++ conditionals/Example1.java
@@ -0,0 +1,18 @@
+package conditionals;
+
+
+public class Example1 {
+    public static void main(String[] args) {
+        int a = 4;
+        boolean b = a == 4;
+        
+        if (b) {
+            System.out.println("It's true!");
+        }        
+
+        if (a == 4) {
+            System.out.println("Ohhh! So a is 4!");
+        }
+
+    }
+}
