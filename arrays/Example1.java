a7febc9712fa25827e762c19ba37cb04e4a5ac3d
Mohamed Maskoul <maskoul.mohammed@gmail.com>
2022-09-07 19:53:39 +0100

    Add arrays examples

---
+++ arrays/Example1.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[10];
+        arr[0] = 4;
+        arr[1] = arr[0] + 5;
+        System.out.println(arr.length);
+    } 
+}
